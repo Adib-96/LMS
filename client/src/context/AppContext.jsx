d32ff3401d073dcdd7d019683a29e8a0643502be
Adib-96 <adibbh1996@gmail.com>
2025-04-30 06:12:15 +0100

    adding courses to UI and handle logic

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
+import humanizeDuration from 'humanize-duration'
 
 export const AppContext = createContext();
 
@@ -35,13 +36,42 @@ export const AppContextProvider = (props) => {
         return totalRating / course.courseRatings.length;
     }
 
+    //function to calculate course chapter time 
+    const calculateChapterTime = (chapter) => {
+        let time  = 0;
+        chapter.chapterContent.map((lecture)=> time+= lecture.lectureDuration);
+        return humanizeDuration(time * 60 * 1000,{units:['h',"m"]});
+    }
+
+    // fnction to calculate course dration
+    const calculateCourseDuration = (course) => {
+        let time = 0;
+
+        course.courseContent.map((chapter)=> chapter.chapterContent.map((lecture)=> 
+        time += lecture.lectureDuration
+        ))
+        return humanizeDuration(time * 60 * 1000,{units:['h',"m"]});
+    }
+
+    //function calculate to ni of lectures in the corse
+
+    const calculateNuOfLectures = (course)=> {
+        let totalLectures = 0;
+        course.courseContent.forEach(chapter => {
+            if(Array.isArray(chapter.chapterContent)) {
+                totalLectures +=chapter.chapterContent.length
+            }
+        });
+        return totalLectures;
+    }
+
 
     useEffect(() => {
-         fetchAllCourses();
+        fetchAllCourses(); 
     },[]);
     
     const value = {
-        currency,allCourses,navigate,calculateRating,isEducator,setEducator
+        currency,allCourses,navigate,calculateRating,isEducator,setEducator,calculateChapterTime,calculateCourseDuration,calculateNuOfLectures
     };
     
     return (
--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,13 +1,136 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
+import Loading from "../../components/student/Loading";
+import { assets } from "../../assets/assets";
+import humanizeDuration from "humanize-duration";
 
 const CourseDetails = () => {
-  return (
-    <div>
-        <h1>
-        coursDetails
-        </h1>
-    </div>
-  )
-}
+  const { id } = useParams();
 
-export default CourseDetails
+  const [courseData, setCourseData] = useState(null);
+  const {
+    allCourses,
+    calculateRating,
+    calculateChapterTime,
+    calculateCourseDuration,
+    calculateNuOfLectures,
+  } = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+
+  return courseData ? (
+    <>
+      <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+        <div className="absolute top-0 left-0 w-full h-full -z-1 bg-gradient-to-b from-cyan-100/70"></div>
+
+        <div className="max-w-xl z-10 text-gray-500">
+          <h1 className="text-2xl font-semibold text-gray-800">
+            {courseData?.courseTitle}
+          </h1>
+          <p
+            className="pt-4 md:text-base text-sm"
+            dangerouslySetInnerHTML={{
+              __html: courseData.courseDescription.slice(0, 200),
+            }}
+          ></p>
+
+          {/*review rating */}
+          <div className="flex  items-center space-x-2 pt-3 pb-1 text-sm">
+            <p>{calculateRating(courseData)}</p>
+            <div className="flex">
+              {[...Array(5)].map((_, i) => (
+                <img
+                  key={i}
+                  src={
+                    i < Math.floor(calculateRating(courseData))
+                      ? assets.star
+                      : assets.star_blank
+                  }
+                  alt=""
+                  className="w-3.5 h-3.5"
+                />
+              ))}
+            </div>
+            <p className="text-gray-500">
+              ({courseData.courseRatings.length}{" "}
+              {courseData.courseRatings.length > 1 ? "ratings" : "rating"})
+            </p>
+            <p>
+              {courseData.enrolledStudents.length}{" "}
+              {courseData.enrolledStudents.length > 1 ? "students" : "student"}
+            </p>
+          </div>
+
+          <p className="text-sm">
+            Course by <span className="text-blue-600 underline">Adibos</span>
+          </p>
+
+          <div className="pt-8 text-gray-800">
+            <h2 className="text--xl font-semibold">Course structure</h2>
+            <div className="pt-5">
+              {courseData.courseContent.map((chapter, index) => (
+                <div
+                  key={index}
+                  className="border border-gray-300 bg-white mb-2 rounded"
+                >
+                  <div className="flex items-center justify-between px-4 py-3 cursor-pointer select-none">
+                    <div className="flex items-center gap-2">
+                      <img src={assets.down_arrow_icon} alt="arrow icon" />
+                      <p className="font-medium md:text-base text-sm">
+                        {chapter.chapterTitle}
+                      </p>
+                    </div>
+                    <p className="text-sm md:text-default">
+                      {chapter.chapterContent.length} lectures -{" "}
+                      {calculateChapterTime(chapter)}
+                    </p>
+                  </div>
+
+                  <div>
+                    <ul>
+                      {chapter.chapterContent.map((lecture, index) => (
+                        <li key={index}>
+                          <img
+                            src={assets.play_icon}
+                            alt="play icon"
+                            className="w-4 h-4 mt-1"
+                          />
+                          <div>
+                            <p>{lecture.lectureTitle}</p>
+                            <dir>
+                              {lecture.isPrevierwFree && <p>Preview</p>}
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] }
+                                )}
+                              </p>
+                            </dir>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+
+        <div></div>
+      </div>
+    </>
+  ) : (
+    <Loading />
+  );
+};
+
+export default CourseDetails;
